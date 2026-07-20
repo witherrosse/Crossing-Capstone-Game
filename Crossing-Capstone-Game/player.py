bd6bc799102ea0d6d46b4db6604aab793716065e
Ali Fooladi <alifooladiwither@gmail.com>
2026-07-20 10:18:31 +0330

    Refactor player class comments and formatting

--- Crossing-Capstone-Game/player.py
+++ Crossing-Capstone-Game/player.py
@@ -4,7 +4,7 @@ STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-### Class to control the player (turtle) ###
+
 
 class Player(Turtle):
 
@@ -13,23 +13,23 @@ class Player(Turtle):
         self.shape("turtle")
         self.color("Black")
         self.penup()
-        self.setheading(90)      # Face upward
+        self.setheading(90)      
         self.goto(STARTING_POSITION)
         self.go_to_start()
 
-    ### Move the turtle forward ###
+    
 
     def move(self):
 
         self.forward(MOVE_DISTANCE)
 
-    ### Send turtle back to starting position ###
+   
 
     def go_to_start(self):
 
         self.goto(STARTING_POSITION)
 
-    ### Check if turtle reached the top ###
+    
 
     def is_at_finish_line(self):
 
