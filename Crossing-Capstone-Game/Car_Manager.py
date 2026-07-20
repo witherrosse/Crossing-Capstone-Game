faea834f75c88e8f7835abd34ed18f409ada8ebc
Ali Fooladi <alifooladiwither@gmail.com>
2026-07-20 10:23:29 +0330

    Refactor comments in CarManager class

--- Crossing-Capstone-Game/Car_Manager.py
+++ Crossing-Capstone-Game/Car_Manager.py
@@ -5,7 +5,7 @@ COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-### Class to manage all cars in the game ###
+
 
 class CarManager:
 
@@ -14,30 +14,34 @@ class CarManager:
         self.all_cars = []
         self.car_speed = STARTING_MOVE_DISTANCE
 
-    ### Create a new car randomly (20% chance each frame) ###
+    
 
     def create_car(self):
+        
+        ''' Create a new car randomly (20% chance each frame) '''
 
         random_chance = random.randint(1, 6)
         if random_chance == 1:
             new_car = Turtle("square")
-            new_car.shapesize(stretch_wid=1, stretch_len=2)   ### Make car shape longer###
+            new_car.shapesize(stretch_wid=1, stretch_len=2)   
             new_car.penup()
             new_car.color(random.choice(COLORS))
             random_y = random.randint(-250, 250)
-            new_car.goto(300, random_y)   # Start from right edge
+            new_car.goto(300, random_y)   
             self.all_cars.append(new_car)
 
-    ### Move all cars to the left ###
+    
 
     def move_cars(self):
+        ''' Move all cars to the left '''
 
         for car in self.all_cars:
 
             car.backward(self.car_speed)
 
-    ### Increase car speed when player levels up ###
+    
 
     def level_up(self):
+        ''' Increase car speed when player levels up ''' 
 
         self.car_speed += MOVE_INCREMENT
